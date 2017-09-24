398769fb7ba1933d579059f07f0fc832c60606f2
miloskuruzovic <miloskuruzovic@gmail.com>
2017-09-24 08:58:30 +0200

    added EER Diagram png and formated article showing view

--- views/article.php
+++ views/article.php
@@ -7,6 +7,26 @@
 
 <div class="container">
 	<?= var_dump($article, $comments) ?>
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<?= $article->title?>
+		</div>
+		<div class="panel-body">
+			<?= $article->text ?>
+		</div>
+		<div class="panel-footer">
+			<?php foreach ($comments as $comment): ?>
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<?= $comment->name ?>
+					</div>
+					<div class="panel-body">
+						<?= $comment->content ?>
+					</div>
+				</div>
+			<?php endforeach ?>
+		</div>
+	</div>
 </div>
 
  <?php 
